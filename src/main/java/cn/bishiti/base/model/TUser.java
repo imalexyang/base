9b00064619f7998da995da24e10a06bc19893e48
杨雅文 <imalex@163.com>
2016-01-10 15:14:06 +0800

    添加memcached缓存-20160110

--- src/main/java/cn/bishiti/base/model/TUser.java
+++ src/main/java/cn/bishiti/base/model/TUser.java
@@ -1,9 +1,16 @@
 package cn.bishiti.base.model;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class TUser {
-    private Integer id;
+public class TUser implements Serializable{
+    /** 
+	* @Fields serialVersionUID : TODO(用一句话描述这个变量表示什么) 
+	*/ 
+	
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
 
     private String name;
 
