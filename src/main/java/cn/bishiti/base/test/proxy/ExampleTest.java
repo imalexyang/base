aa8bd2be688d38b4d2c44b06dde5b78998ecebf9
imalexyang <imalex@163.com>
2016-12-13 22:11:09 +0800

    mutiThread-20161213

--- src/main/java/cn/bishiti/base/test/proxy/ExampleTest.java
+++ src/main/java/cn/bishiti/base/test/proxy/ExampleTest.java
@@ -1,10 +1,6 @@
 package cn.bishiti.base.test.proxy;
 
 import java.lang.reflect.Proxy;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
 
@@ -18,9 +14,7 @@ public class ExampleTest {
 		IHelloWorld helloWorld=(IHelloWorld)Proxy.newProxyInstance(hw.getClass().getClassLoader(), hw.getClass().getInterfaces(), handler);
 		helloWorld.example();
 		
-		ThreadPoolExecutor  poolExecutor=new ThreadPoolExecutor(2, 4, 3, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(3), new ThreadPoolExecutor.DiscardOldestPolicy());
 		
-		//poolExecutor.execute(command);
 	}
 	
 }
