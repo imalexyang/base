dd3e9ba41542778c91be7092e36c76e57923c4d3
imalexyang <imalex@163.com>
2016-12-30 16:21:09 +0800

    just test-20161230

--- src/main/java/cn/bishiti/base/test/Test.java
+++ src/main/java/cn/bishiti/base/test/Test.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Test {
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) throws InterruptedException {
 		List<Integer> list=new ArrayList<Integer>();
 		Provider p=new Provider(list);
@@ -13,5 +14,13 @@ public class Test {
 		new Thread(c).start();
 		Thread.sleep(2000);
 		new Thread(p).start();
+		
+		FatherTest ft=new FatherTest();
+		System.out.println();
+		FatherTest ft2=new FatherTest();
+		System.out.println();
+		SonTest st=new SonTest();
+		System.out.println();
+		SonTest st2=new SonTest();
 	}
 }
