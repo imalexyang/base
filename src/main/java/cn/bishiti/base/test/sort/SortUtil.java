a0d55ca22a6b7564ea87e2abbb3caa14d34ce55e
imalexyang <imalex@163.com>
2017-02-20 09:06:40 +0800

    sortUtil-20170220

--- src/main/java/cn/bishiti/base/test/sort/SortUtil.java
+++ src/main/java/cn/bishiti/base/test/sort/SortUtil.java
@@ -37,7 +37,16 @@ public class SortUtil {
 		return array;
 	}
 	
-	
+	public static int[] insertionSort(int[] array){
+		for(int i=0;i<array.length-1;i++){
+			for(int j=1;j>0&&array[j]<array[j-1];j--){
+				int temp=array[i];
+				array[j]=array[j-1];
+				array[j-1]=temp;
+			}
+		}
+		return array;
+	}
 }
 
   
