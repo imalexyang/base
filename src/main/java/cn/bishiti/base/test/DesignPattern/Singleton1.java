fad0290d5fb4ebd893c1a49ec980d6e94d09dca0
imalexyang <imalex@163.com>
2017-02-20 10:09:18 +0800

    Design Pattern-20170220

---
+++ src/main/java/cn/bishiti/base/test/DesignPattern/Singleton1.java
@@ -0,0 +1,23 @@
+package cn.bishiti.base.test.DesignPattern; 
+
+public class Singleton1 {
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub 
+
+	}
+	
+	private static Singleton1 singleton;
+	
+	private Singleton1(){};
+	
+	public static synchronized Singleton1 getInstance(){
+		if(singleton==null){
+			singleton=new Singleton1();
+		}
+		return singleton;
+	}
+}
+
+  
