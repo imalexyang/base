d77850e9a0c69cda1e91f6f04ef07984ba29a299
imalexyang <imalex@163.com>
2017-02-20 09:46:41 +0800

    sortUtil-20170220

--- src/main/java/cn/bishiti/base/test/sort/SortUtil.java
+++ src/main/java/cn/bishiti/base/test/sort/SortUtil.java
@@ -23,7 +23,7 @@ public class SortUtil {
 
 	
 	public static int[] selectionSort(int[] array){
-		for(int i=0;i<array.length-1;i++){
+		for(int i=0;i<array.length;i++){
 			int min=i;
 			for(int j=i+1;j<array.length;j++){
 				if(array[j]<array[min]){
