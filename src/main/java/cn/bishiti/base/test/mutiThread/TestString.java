eef1502b018f12afa96ca20e12dceb6c96c13f19
imalexyang <imalex@163.com>
2017-02-25 18:17:59 +0800

    join

--- src/main/java/cn/bishiti/base/test/mutiThread/TestString.java
+++ src/main/java/cn/bishiti/base/test/mutiThread/TestString.java
@@ -2,17 +2,25 @@ package cn.bishiti.base.test.mutiThread;
 
 public class TestString {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		StringBuffer sbf=new StringBuffer("sbf");
 		StringBuilder sbd=new StringBuilder("sbd");
 		
-		new Thread(new StringBufferTest(sbf)).start();
-		new Thread(new StringBufferTest(sbf)).start();		
+		Thread t1=new Thread(new StringBufferTest(sbf));
+		Thread t2=new Thread(new StringBufferTest(sbf));		
 		
-		new Thread(new StringBuilderTest(sbd)).start();
-		new Thread(new StringBuilderTest(sbd)).start();
+		Thread t3=new Thread(new StringBuilderTest(sbd));
+		Thread t4=new Thread(new StringBuilderTest(sbd));
 		
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+		
+		t1.join();
+		
+		System.out.println("abc");
 		//验证StringBuffer 和StringBuilder 线程安全性
 	}
 
