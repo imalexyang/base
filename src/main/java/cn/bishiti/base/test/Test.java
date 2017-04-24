860aa3d57bc66d221ca4d4c26d44d0dc0524c8c5
imalexyang <imalex@163.com>
2017-04-24 23:22:39 +0800

    print stack info

--- src/main/java/cn/bishiti/base/test/Test.java
+++ src/main/java/cn/bishiti/base/test/Test.java
@@ -2,7 +2,20 @@ package cn.bishiti.base.test;
 
 public class Test {
 
-	public static void main(String[] args) throws InterruptedException {
-		
+	public static void main(String[] args) throws InterruptedException {		
+		Throwable ex = new Throwable();
+        StackTraceElement[] stackElements = ex.getStackTrace();
+        if (stackElements != null) {
+            for (int i = 0; i < stackElements.length; i++) {
+                System.out.print(stackElements[i].getClassName()+"/ ");
+                System.out.print(stackElements[i].getFileName()+"/ ");
+                System.out.print(stackElements[i].getLineNumber()+"/ ");
+                System.out.println(stackElements[i].getMethodName());
+                System.out.println("-----------------------------------");
+            }
+        }
+        
+        Exception e = new Exception("this is a log");
+        e.printStackTrace();
 	}
 }
