9317650f8355f8cf66926b38ac5d6806192a4861
imalexyang <imalex@163.com>
2017-05-16 23:04:38 +0800

    composite

---
+++ src/main/java/cn/bishiti/base/test/DesignPattern/Composite/Tree.java
@@ -0,0 +1,22 @@
+package cn.bishiti.base.test.DesignPattern.Composite;
+
+public class Tree {
+
+	TreeNode root=null;
+	
+	public Tree(String name){
+		root=new TreeNode(name);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Tree tree=new Tree("A");
+		TreeNode nodeB=new TreeNode("B");
+		TreeNode nodeC=new TreeNode("C");
+		
+		nodeB.add(nodeC);
+		tree.root.add(nodeB);
+		System.out.println("build the tree finished.");
+	}
+
+}
---
+++ src/main/java/cn/bishiti/base/test/DesignPattern/Composite/TreeNode.java
@@ -0,0 +1,40 @@
+package cn.bishiti.base.test.DesignPattern.Composite;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class TreeNode {
+
+	private String name;
+	private TreeNode parent;
+	private Vector<TreeNode> children=new Vector<TreeNode>();
+	
+	public TreeNode(String name){
+		this.name=name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public TreeNode getParent() {
+		return parent;
+	}
+	public void setParent(TreeNode parent) {
+		this.parent = parent;
+	}
+	
+	public void add(TreeNode node){
+		children.add(node);
+	}
+	
+	public void remove(TreeNode node){
+		children.remove(node);
+	}
+	
+	public Enumeration<TreeNode> getChildren(){
+		return children.elements();
+	}
+}
