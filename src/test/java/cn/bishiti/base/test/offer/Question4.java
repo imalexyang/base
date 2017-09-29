29b76acd643bdb57d4ff620cefdb1d5f893333ca
Alex Yang <imalex@163.com>
2017-09-29 16:28:12 +0800

    Update Question4.java
    done q4

--- src/test/java/cn/bishiti/base/test/offer/Question4.java
+++ src/test/java/cn/bishiti/base/test/offer/Question4.java
@@ -2,9 +2,21 @@ package cn.bishiti.base.test.offer;
 
 public class Question4 {
 
-	public static void main(String[] args) {
-		
-
+	public static void main(String[] args) {		
+		String str = "We are happy.";		
+		if(str.indexOf(" ")<0){
+			System.out.println("没有空格,后边的可以跳过.");
+		}
+		String[] arraySource = str.split("");
+		StringBuffer sb = new StringBuffer();
+		for(String s:arraySource){
+			if(s.equals(" ")){
+				sb.append("%20");
+			}else{
+				sb.append(s);
+			}			
+		}
+		System.out.println(sb);
 	}
 
 }
